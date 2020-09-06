285e31d9dd5c46ccf27395563d9e4c530390f261
Nuradin Ismail <nuradin170@gmail.com>
2020-09-06 07:49:09 +0300

    Completed UI functionalities

--- client/src/componants/pages/Home.js
+++ client/src/componants/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Contacts from '../contact/Contacts';
 import ContactForm from '../contact/ContactForm';
+import ContactFilter from '../contact/ContactFilter';
 const Home = () => {
   return (
     <div className="grid-2">
@@ -8,6 +9,7 @@ const Home = () => {
         <ContactForm />
       </div>
       <div>
+        <ContactFilter />
         <Contacts />
       </div>
     </div>
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -38,6 +38,7 @@ const ContactState = (props) => {
       },
     ],
     current: null,
+    filtered: null,
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -58,25 +59,36 @@ const ContactState = (props) => {
   };
 
   // Clear Current Contact
-  const clearCurrent = (contact) => {
+  const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
   };
 
   // Update Contact
-
+  const updateContact = (contact) => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  };
   // Filter Contacts
-
+  const filterContacts = (text) => {
+    dispatch({ type: FILTER_CONTACTS, payload: text });
+  };
   // Clear Filter
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER });
+  };
 
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
         current: state.current,
+        filtered: state.filtered,
         addContact,
         deleteContact,
         setCurrent,
         clearCurrent,
+        updateContact,
+        filterContacts,
+        clearFilter,
       }}
     >
       {props.children}
